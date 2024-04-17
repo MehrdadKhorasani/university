c89036b008916ff9e35255eae45ea91bcb0717f5
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-04-17 22:32:21 +0330

    heap array refactor

--- First Semester/Algorithms/2- HEAP arr.py
+++ First Semester/Algorithms/2- HEAP arr.py
@@ -1,46 +1,24 @@
-# arr = [19, 48, 10, 2, 16, 5 , 33, 32]
-
-
-# def heap(arr):
-#   heap_arr = []
-#   for i in arr:
-#     heap_arr.append(i)
-#     index = heap_arr.index(i)
-#     parent_index = int(heap_arr.index(i) / 2)
-#     if(heap_arr[parent_index] < heap_arr[index]):
-#       temp = heap_arr[index]
-#       heap_arr[index] = heap_arr[parent_index]
-#       heap_arr[parent_index] = temp
-
-#   print(heap_arr)
-# heap(arr)
-
 def heapify(arr, n, i):
-    smallest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+    largest = i
+    l = 2 * i + 1 
+    r = 2 * i + 2  
 
-    if left < n and arr[left] < arr[smallest]:
-        smallest = left
+    if l < n and arr[i] < arr[l]:
+        largest = l
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+    if largest != i:
+        (arr[i], arr[largest]) = (arr[largest], arr[i]) 
+        heapify(arr, n, largest)
 
-    if right < n and arr[right] < arr[smallest]:
-        smallest = right
-
-    if smallest != i:
-        arr[i], arr[smallest] = arr[smallest], arr[i]
-        heapify(arr, n, smallest)
-
-def build_min_heap(arr):
+def heapSort(arr):
     n = len(arr)
-    for i in range(n // 2 - 1, -1, -1):
+    for i in range(n // 2, -1, -1):
         heapify(arr, n, i)
-
-def heap_sort(arr):
-    build_min_heap(arr)
-    for i in range(len(arr) - 1, 0, -1):
-        arr[0], arr[i] = arr[i], arr[0]
+    for i in range(n - 1, 0, -1):
+        (arr[i], arr[0]) = (arr[0], arr[i]) 
         heapify(arr, i, 0)
-
-arr = [19, 48, 10, 2, 16, 5, 33, 32]
-heap_sort(arr)
-print(arr)
+ 
+arr = [89, 23, 14 , 16, 62, 75, 1, 12, 17, 33, 55, 97]
+heapSort(arr)
+print(arr) #OUTPUT
