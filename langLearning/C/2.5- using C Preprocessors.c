48a139f7cb4758c266d52a1d41ef9875d11cb332
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-05-10 22:55:30 +0330

    c programming

---
+++ langLearning/C/2.5- using C Preprocessors.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define COUNT 10
+#define GREETING "Type a positive value: "
+int main()
+{
+    printf(GREETING);
+    for(int a=0;a<b;a++){
+        printf("I must do this %d times\n", COUNT);
+        if(a==9)
+            break;
+    }
+    return 0;
+}
