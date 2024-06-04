3f0a04670eb9a97c1d6e3ab4b66eab9a8686ffa1
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-06-04 20:14:16 +0330

    insertion sort for python

---
+++ First Semester/Algorithms/7- insertion sort.py
@@ -0,0 +1,15 @@
+# Insertion Sort
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j] :
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+arr = [9, 3, 7, 1, 5, 6, 2, 4, 8]
+insertionSort(arr)
+print(arr)
+
