906fc5728429763a81c3e8baa71a884e1418f7fc
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-07-29 20:09:13 +0330

    open and edit file

--- langLearning/PYTHON/Mehri/19.py
+++ langLearning/PYTHON/Mehri/19.py
@@ -1,4 +1,14 @@
 import os
-path = r".\test"
-if os.path.exists(path):
-  os.mkdir(path)
+os.getcwd()
+if os.path.exists("students.txt"):
+  openFile = open("students.txt", mode= 'r',encoding="utf-8")
+
+  print("pointer:", openFile.tell())
+  for line in openFile:
+    print(line,end="")
+
+  openFile.seek(0)
+  print(openFile.readline())
+  print("pointer:", openFile.tell())
+
+  openFile.close()
