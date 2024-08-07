226e0fcfc5334ab8610a5bcc32369556b38754d6
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-08-07 17:01:09 +0330

    init commit for anvari course: C

---
+++ langLearning/C/MaktabKhoone - Ahmad Anvari (sharif)/preface/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /*
+  //// sum of two numbers:
+  int number1 = 12;
+  int number2 = 71;
+  // unsigned or signed variables:
+  unsigned int result;
+  // constanst variable:
+  // const int admin_name;
+  // admin_name = 7;
+  //error: assignment of read-only variable 'admin_name'|
+  result = number1 + number2;
+  printf("%d\n",result);
+  */
+  /*
+  //// Polynomial:
+  //// X = 1/2 gt^2 + V0t + x0
+  const float g = 9.8;
+  unsigned int t = 4;
+  float v0 = 2.3;
+  float x0 = 4.5;
+  float x;
+  x = g * t * t / 2 + v0 * t + x0;
+  printf("%f", x);
+  */
+  /*
+  //// Char:
+  char my_character = 'A'; // can hold just one character
+  int my_ascii = my_character;
+  //represent by single quotation
+  printf("The value is: %c\n", my_character); //=> A
+  printf("The ascii value is: %d", my_ascii); //=> 65
+
+  //// convert int to char:
+  // int value must be less than 255, because char type cannot hold more than 254 value
+  int value = 107;
+  char k_character = value;
+  printf("\n%c", k_character); //=> k
+  */
+  return 0;
+}
