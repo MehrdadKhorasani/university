9ef1f0a074a943d97c8afe0123261f73c88f486f
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-08-08 17:59:38 +0330

    add condition section: C

---
+++ langLearning/C/MaktabKhoone - Ahmad Anvari (sharif)/CondiLoops/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /*
+  //// IF statement
+  // operators in conditions expressions: (== , != , > , < , >= , <=)
+  //1.
+  int age;
+  puts("Please enter your age:1");
+  scanf("%d", &age);
+  if (age > 18) {
+    printf("%d", age);
+  }
+  //2.
+  char c = getchar();
+  if (c == 'E') // ATTENTION: Use Single Quote
+  {
+    puts("Equal");
+  }
+  puts("end");
+  */
+
+  return 0;
+}
