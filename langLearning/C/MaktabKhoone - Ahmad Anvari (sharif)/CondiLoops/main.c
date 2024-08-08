118c82d188fbe337c21833fdf0a99341ecc7a22a
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-08-08 23:26:27 +0330

    if statements & logical operators: C

--- langLearning/C/MaktabKhoone - Ahmad Anvari (sharif)/CondiLoops/main.c
+++ langLearning/C/MaktabKhoone - Ahmad Anvari (sharif)/CondiLoops/main.c
@@ -20,7 +20,26 @@ int main()
     puts("Equal");
   }
   puts("end");
+  //3.
+  int age;
+  puts("please enter your age");
+  scanf("%d", &age);
+  if (age > 18) puts("You're an adult");
+  else puts("go back after you turn 18");
+  //// Logical operations (! , && , ||)
+  //4.
+  int age;
+  puts("please Enter your age: ");
+  scanf("%d",&age);
+  if(age <= 5) puts("You're a kid");
+  else if(age > 5 && age < 18) puts("you're under legal age");
+  else puts("You're an adult.");
+  //5.
+  char ch;
+  puts("Please enter a character");
+  ch = getchar();
+  if (!(ch >= 'a' && ch <= 'z')) puts("your letter is uppercase");
+  else puts("your letter is lowercase");
   */
-
   return 0;
 }
