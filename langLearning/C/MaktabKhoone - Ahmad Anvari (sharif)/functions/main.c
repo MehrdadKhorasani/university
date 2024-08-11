ab0aff634a466b3360f739fc6b694d0449e98b86
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-08-11 13:24:51 +0330

    start the function section: C

---
+++ langLearning/C/MaktabKhoone - Ahmad Anvari (sharif)/functions/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+//// SELF CODING CHALLENGE: SUM FUNCTION:
+int sum(nums){
+  int num, result;
+  int ac[10] = {};
+  for(int i = 1; i <= nums; i++){
+    printf("number %d of %d \n", i, nums);
+    scanf("%d", &num);
+    ac[i - 1] = num;
+  }
+  for(int i = 1; i <= nums; i++){
+    result += ac[i - 1];
+  }
+  return result;
+}
+
+int main()
+{
+  int nums;
+  puts("How many numbers do you want to sum? (1-10)");
+  scanf("%d", &nums);
+  if(nums > 10) return;
+  int result = sum(nums);
+  printf("The sums of your numbers is: %d", result);
+  return 0;
+}
+*/
+
+int main()
+{
+  printf("Hello, World!");
+  return 0;
+}
