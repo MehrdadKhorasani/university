8ca3441af4bd1088c8286793b8ff5bdc4d0265b9
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-08-12 13:44:14 +0330

    function section practices: C

--- langLearning/C/MaktabKhoone - Ahmad Anvari (sharif)/functions/main.c
+++ langLearning/C/MaktabKhoone - Ahmad Anvari (sharif)/functions/main.c
@@ -27,9 +27,62 @@ int main()
   return 0;
 }
 */
+/*
+//// fn for compare 3 integer for max value:
+int max3(int value1, int value2, int value3){
+  if(value1 > value2)
+  {
+    if(value1 > value3){
+      return value1;
+    }
+    else
+    {
+      return value3;
+    }
+  }
+  else
+  {
+    if(value2 > value3)
+    {
+      return value2;
+    }
+    else
+    {
+      return value3;
+    }
+  }
+}
+int main()
+{
+  int w1, w2, w3;
+  int h1, h2, h3;
+  scanf("%d %d %d", &w1, &w2, &w3); //get 3 weight from user
+  scanf("%d %d %d", &h1, &h2, &h3); //get 3 height from user
+  int max_w = max3(w1,w2,w3); // calculate max weight
+  int max_h = max3(h1,h2,h3); // calculate max height
+  printf("max width: %d \n max height: %d", max_w, max_h);
+  return 0;
+}
+
+*/
+/*
+void new_function(){
+  int counter = 0;
+  counter++;
+  printf("%d\n",counter);
+}
+int main()
+{
+  // printf("%d",counter); //ERROR: scoping error
+  new_function(); //=> 1
+  new_function(); //=> 1
+  // every time new_function called, a new variable (called counter) is made in stack.
+  return 0;
+}
+*/
 
 int main()
 {
-  printf("Hello, World!");
+
   return 0;
 }
