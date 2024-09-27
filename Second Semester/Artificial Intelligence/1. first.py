69f5f4fa879c05189824f5a2ca8d8efb1d47e8ad
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-09-27 13:23:05 +0330

    first practice: fresh look to python lang

--- Second Semester/Artificial Intelligence/1. first.py
+++ Second Semester/Artificial Intelligence/1. first.py
@@ -1,17 +1,43 @@
-# def my_final_grade_calculation(file_name):
-#   return {"tom": "pass", "mary":"pass", "joan": "fail"}
+def my_final_grade_calculation(txt_list):
 
-# def main():
-#   pass
+  result_dict = {}
 
-# if __name__ == "__main__":
-#   main()
+  name = txt_list[0].lower()
+  quizes = []
+  assignments = []
+  midterm = int(txt_list[11])
+  final = int(txt_list[12])
+  
+  for i in range(11):
+      if 0 < i < 7:
+          quizes.append(int(txt_list[i]))
+      elif i >= 7:
+          assignments.append(int(txt_list[i]))
 
-text_file = open("data.txt", "r")
+  quizes.remove(min(quizes))
+  quizes.remove(min(quizes))
+  quizes_avg = int(sum(quizes) / len(quizes))
 
-txt_list = []
+  assignments.remove(min(assignments))
+  assignments_avg = int(sum(assignments) / len(assignments))
 
-for line in text_file:
-  txt_list.append(line.split(','))
+  score = (quizes_avg + assignments_avg + midterm + final) / 4
+  pass_or_not = "pass" if score >= 60 else "fail"
 
-print(txt_list)
+  result_dict[name] = pass_or_not
+
+  return result_dict
+
+def main():
+  final_results = {}
+
+  with open("data.txt", "r") as text_file:
+      for line in text_file:
+          txt_list = line.strip().split(',')
+          student_result = my_final_grade_calculation(txt_list)
+          final_results.update(student_result)
+
+  print(final_results)
+
+if __name__ == "__main__":
+  main()
