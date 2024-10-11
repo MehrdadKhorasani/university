a82137a709ce94153f4fcbc4dad5f433309c1579
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-10-11 14:53:09 +0330

    Public and Private modifires

--- langLearning/TS/Colt Steele course/Classes/dist/index.js
+++ langLearning/TS/Colt Steele course/Classes/dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("Bitch");
+class Player {
+    constructor(first, last) {
+        this.score = 0;
+        this.first = first;
+        this.last = last;
+    }
+    secretMethod() {
+        console.log("Secret");
+    }
+}
+const elton = new Player("Elton", "Steele");
+// elton.first = "John"; // Error
--- langLearning/TS/Colt Steele course/Classes/src/index.ts
+++ langLearning/TS/Colt Steele course/Classes/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Bitch");
+class Player {
+  public readonly first: string;
+  public readonly last: string;
+  private score: number = 0;
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+  private secretMethod() {
+    console.log("Secret");
+  }
+}
+
+const elton = new Player("Elton", "Steele");
+// elton.first = "John"; // Error
