2d0bf12073f95b20eba1dae87f4c861d1edb2d1f
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-10-12 11:54:45 +0330

    ts Classes

--- langLearning/TS/Colt Steele course/Classes/dist/index.js
+++ langLearning/TS/Colt Steele course/Classes/dist/index.js
@@ -1,13 +1,114 @@
 "use strict";
+/*
 class Player {
-    constructor(first, last) {
-        this.score = 0;
+  public readonly first: string;
+  public readonly last: string;
+  private score: number = 0;
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+  private secretMethod() {
+    console.log("Secret");
+  }
+}
+
+const elton = new Player("Elton", "Steele");
+// elton.first = "John"; // Error
+*/
+/*
+class Player {
+  private score: number = 0;
+  constructor(public first: string, public last: string) {}
+  private secretMethod() {
+    console.log("Secret");
+  }
+}
+const elton = new Player("Elton", "Steele");
+console.log(elton); //=> Player {first: 'Elton', last: 'Steele', score: 0}
+*/
+///// Getters & Setters
+class Player {
+    constructor(first, last, 
+    // private _score: number
+    _score) {
         this.first = first;
         this.last = last;
+        this._score = _score;
     }
     secretMethod() {
         console.log("Secret");
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0)
+            throw new Error("Score can not be negative");
+        this._score = newScore;
+    }
 }
-const elton = new Player("Elton", "Steele");
-// elton.first = "John"; // Error
+const elton = new Player("Elton", "Steele", 0);
+console.log(elton.fullName); //=> Elton Steele
+console.log(elton.score); //=> 0
+elton.score = 99;
+console.log(elton.score); //=> 99
+class SuperPlayer extends Player {
+    constructor() {
+        super(...arguments);
+        this.isAdmin = true;
+    }
+    maxScore() {
+        this._score = 99999;
+        // unlike the private, protected types are accessible in child classes.
+    }
+}
+class Bike {
+    constructor(color) {
+        this.color = color;
+    }
+}
+class Jacket {
+    constructor(brand, color) {
+        this.brand = brand;
+        this.color = color;
+    }
+    print() {
+        console.log(`%{this.color} ${this.brand} jacket`);
+    }
+}
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("Zara", "Brown");
+///// Abstract Classes
+class Employee {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+class FullTimeEmployee extends Employee {
+    constructor(firstName, lastName, salary) {
+        super(firstName, lastName);
+        this.salary = salary;
+    }
+    getPay() {
+        return this.salary;
+    }
+}
+class PartTimeEmployee extends Employee {
+    constructor(firstName, lastName, hourlyRate, hoursWorked) {
+        super(firstName, lastName);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+    getPay() {
+        return this.hourlyRate * this.hoursWorked;
+    }
+}
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay()); //=> 95000
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay()); //=> 26400
--- langLearning/TS/Colt Steele course/Classes/src/index.ts
+++ langLearning/TS/Colt Steele course/Classes/src/index.ts
@@ -1,3 +1,4 @@
+/*
 class Player {
   public readonly first: string;
   public readonly last: string;
@@ -13,3 +14,108 @@ class Player {
 
 const elton = new Player("Elton", "Steele");
 // elton.first = "John"; // Error
+*/
+
+/*
+class Player {
+  private score: number = 0;
+  constructor(public first: string, public last: string) {}
+  private secretMethod() {
+    console.log("Secret");
+  }
+}
+const elton = new Player("Elton", "Steele");
+console.log(elton); //=> Player {first: 'Elton', last: 'Steele', score: 0}
+*/
+
+///// Getters & Setters
+class Player {
+  constructor(
+    public first: string,
+    public last: string,
+    // private _score: number
+    protected _score: number
+  ) {}
+  private secretMethod() {
+    console.log("Secret");
+  }
+  get fullName(): string {
+    return `${this.first} ${this.last}`;
+  }
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) throw new Error("Score can not be negative");
+    this._score = newScore;
+  }
+}
+
+const elton = new Player("Elton", "Steele", 0);
+console.log(elton.fullName); //=> Elton Steele
+console.log(elton.score); //=> 0
+elton.score = 99;
+console.log(elton.score); //=> 99
+
+class SuperPlayer extends Player {
+  public isAdmin: boolean = true;
+  maxScore() {
+    this._score = 99999;
+    // unlike the private, protected types are accessible in child classes.
+  }
+}
+
+///// Classes & Interfaces
+interface Colorful {
+  color: string;
+}
+interface Printable {
+  print(): void;
+}
+class Bike implements Colorful {
+  constructor(public color: string) {}
+}
+class Jacket implements Colorful, Printable {
+  constructor(public brand: string, public color: string) {}
+  print() {
+    console.log(`%{this.color} ${this.brand} jacket`);
+  }
+}
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("Zara", "Brown");
+
+///// Abstract Classes
+abstract class Employee {
+  constructor(public firstName: string, public lastName: string) {}
+  abstract getPay(): number;
+}
+
+class FullTimeEmployee extends Employee {
+  constructor(firstName: string, lastName: string, private salary: number) {
+    super(firstName, lastName);
+  }
+  getPay(): number {
+    return this.salary;
+  }
+}
+class PartTimeEmployee extends Employee {
+  constructor(
+    firstName: string,
+    lastName: string,
+    private hourlyRate: number,
+    private hoursWorked: number
+  ) {
+    super(firstName, lastName);
+  }
+
+  getPay(): number {
+    return this.hourlyRate * this.hoursWorked;
+  }
+}
+
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay()); //=> 95000
+
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay()); //=> 26400
